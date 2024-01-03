911c2799e95ec576c4f28f15e7cef2674f430972
Buh <serbuh@gmail.com>
2024-01-03 22:16:39 +0200

    refactor interface_creator (order, funcitons)

--- interface_creator/interface_creator.py
+++ interface_creator/interface_creator.py
@@ -18,7 +18,7 @@ class InterfaceCreatorGUI():
             return
 
         # Get list of interface examples
-        self.get_files_from_folder(self.interfaces_folder)
+        self.file_paths, self.filenames_only = self.get_files_from_folder(self.interfaces_folder)
         
         if len(self.file_paths) == 0:
             sg.popup(f'No config files in folder {self.interfaces_folder}')
@@ -31,7 +31,7 @@ class InterfaceCreatorGUI():
         row_port_selection = [
             [sg.Text('Select a port'),
                 sg.Input('', enable_events=True, key='-PORT_INPUT-', expand_x=True, justification='left'),
-                sg.Button("Update")],
+                sg.Button("Update Port")],
         ]
 
         # File list column
@@ -41,7 +41,7 @@ class InterfaceCreatorGUI():
             [sg.Button('Prev', size=(8, 2)), sg.Button('Next', size=(8, 2)),
                 sg.Text('File 1 of {}'.format(len(self.file_paths)), size=(15, 1), key='-FILENUM-')]
         ]
-
+        
         # define layout, show and read the window
         col_file_content = [
             [sg.Text("Select something", key='-FILENAME-')],
@@ -54,71 +54,79 @@ class InterfaceCreatorGUI():
         self.window = sg.Window('Config Generator', layout, return_keyboard_events=True, use_default_focus=False)
     
     def loop(self):
+                    
         # loop reading the user input and displaying text, filepath
         filenum, filepath, filename = 0, self.file_paths[0], self.filenames_only[0]
         while True:
-
             event, values = self.window.read()
+            #print(event)
 
-            # --------------------- Button & Keyboard ---------------------
+            # --------------------- Window ---------------------
             if event == sg.WIN_CLOSED:
                 break
+            # --------------------- Buttons ---------------------
             elif event in ('Next', 'MouseWheel:Down', 'Down:40', 'Next:34') and filenum < len(self.file_paths)-1:
                 filenum += 1
                 filepath = os.path.join(self.interfaces_folder, self.filenames_only[filenum])
                 self.window['-LISTBOX-'].update(set_to_index=filenum, scroll_to_index=filenum)
+                self.update_selected_filename(filenum, filepath)
             elif event in ('Prev', 'MouseWheel:Up', 'Up:38', 'Prior:33') and filenum > 0:
                 filenum -= 1
                 filepath = os.path.join(self.interfaces_folder, self.filenames_only[filenum])
                 self.window['-LISTBOX-'].update(set_to_index=filenum, scroll_to_index=filenum)
-            elif event == 'Exit':
-                break
+                self.update_selected_filename(filenum, filepath)
             elif event == '-LISTBOX-':
                 filepath = os.path.join(self.interfaces_folder, values['-LISTBOX-'][0])
                 filenum = self.file_paths.index(filepath)
-            
-            # Generate
-            if event == 'Generate':
+                self.update_selected_filename(filenum, filepath)
+            elif event == 'Generate':
                 self.popup_generate(self.window['-MULTILINE-'].get())
-            
-            # Get file name (w/o path)
-            filename = self.filenames_only[filenum]
-
+            elif event == 'Update Port':
+                port = self.window["-PORT_INPUT-"].get()
+                print(port)
+            elif event == 'Exit':
+                break
             # ----------------- Menu choices -----------------
-            if event == 'Open Folder':
+            elif event == 'Open Folder':
                 newfolder = sg.popup_get_folder('New folder', no_window=True)
                 if newfolder is None:
                     continue
                 
-                self.get_files_from_folder(newfolder)
+                self.file_paths, self.filenames_only = self.get_files_from_folder(newfolder)
 
                 self.window['-LISTBOX-'].update(values=self.filenames_only)
                 self.window.refresh()
 
                 filenum = 0
+
             elif event == 'About':
                 sg.popup('Wa wa wee wa',
                         'Very nice!')
 
-            # Update multiline with the text
-            try:
-                with open(filepath, "rt", encoding='utf-8') as f:
-                    text = f.read()
-                self.window['-MULTILINE-'].update(text)
-            except Exception as e:
-                print("Error: ", e)
-                
-            # update window with filepath
-            self.window['-FILENAME-'].update(filename)
-
-            # update page display
-            self.window['-FILENUM-'].update('File {} of {}'.format(filenum + 1, len(self.file_paths)))
-
         self.window.close()
+    
+    def update_selected_filename(self, filenum, filepath):
+        # Get file name (w/o path)
+        filename = self.filenames_only[filenum]
+
+        # update window with filepath
+        self.window['-FILENAME-'].update(filename)
+
+        # update page display
+        self.window['-FILENUM-'].update('File {} of {}'.format(filenum + 1, len(self.file_paths)))
+
+        # Update multiline with the text
+        try:
+            with open(filepath, "rt", encoding='utf-8') as f:
+                text = f.read()
+            self.window['-MULTILINE-'].update(text)
+        except Exception as e:
+            print("Error: ", e)
 
     def get_files_from_folder(self, interfaces_folder):
-        self.file_paths = [os.path.join(interfaces_folder, f) for f in os.listdir(interfaces_folder) if f.lower().endswith('.txt')]
-        self.filenames_only = [f for f in os.listdir(interfaces_folder) if f.lower().endswith('.txt')]
+        file_paths = [os.path.join(interfaces_folder, f) for f in os.listdir(interfaces_folder) if f.lower().endswith('.txt')]
+        filenames_only = [f for f in os.listdir(interfaces_folder) if f.lower().endswith('.txt')]
+        return file_paths, filenames_only
 
     def popup_generate(self, text):
         layout = [
