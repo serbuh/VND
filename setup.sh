927aefd5987018413d3338445bd36ad468b7e7b1
Sergey <a@a.com>
2024-01-08 13:28:24 +0200

    add comments to setup file

--- setup.sh
+++ setup.sh
@@ -6,4 +6,22 @@ docker exec -it -w $PWD vnd-docker ./run.sh
 
 read -p "Press enter to kill container"
 docker container rm -f vnd-docker
+exit 0
+
+# Build image from Dockerfile
+docker build -t vnd-docker-2024-01-07 .
+
+# Save image to tar
+docker save -o vnd-docker-v1.tar vnd-docker-2024-01-07
+
+or 
+
+docker save vnd-docker-2024-01-07 | gzip > vnd.tgz
+
+# Install image from tar (On target)
+docker load -i vnd-docker-v1.tar
+
+or 
+
+gunzip -c vnd.tgz | docker load
 
