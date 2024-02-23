07ecd138710dba76b2d264dcfbb673b768f3c608
Buh <serbuh@gmail.com>
2024-02-23 15:25:20 +0200

    Put flask and its event handlers in a class

--- telemetry_server/server.py
+++ telemetry_server/server.py
@@ -9,46 +9,10 @@ from gevent import monkey
 
 monkey.patch_all()
 
+# Paths
 script_folder = os.path.dirname(os.path.abspath(__file__))
-
 openmct_dist = os.path.join(script_folder, "..", "openmct", "dist")
-app = Flask(__name__, static_url_path='', static_folder=openmct_dist, template_folder='templates')
 
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, async_mode="gevent")
-
-historic_data = []
-subscribed_keys = {}
-historic_data_max_size = 1000
-
-@socketio.on("subscribe")
-def handle_subscribe(key):
-    print(f"Subscribe req: {key}")
-    subscribed_keys[key] = True
-
-@socketio.on("unsubscribe")
-def handle_unsubscribe(key):
-    print(f"Unsubscribe req: {key}")
-    subscribed_keys.pop(key, None)
-
-@app.route('/')
-def static_file():
-    return app.send_static_file('index.html')
-
-@app.route('/history/<key>/<start>/<end>/<strategy>/<size>')
-def handle_historic_data(key, start, end, strategy, size):
-    historic_blob = []
-    for msg_batch in historic_data:
-        if msg_batch["timestamp"] > float(start) and msg_batch["timestamp"] < float(end):
-            for stored_key, value in msg_batch.items(): # Extract the item
-                if stored_key == key: # Find needed key
-                    if isinstance(value, list): # Handle tuples in historic. Take only the first value
-                        value = value[0]
-                    historic_msg = {"id": stored_key, "value": value, "timestamp": msg_batch["timestamp"], "mctLimitState":None}
-                    # print(f"One of the historic msgs: {historic_msg}")
-                    historic_blob.append(historic_msg)
-    print(f"Send {len(historic_blob)} history items for {key}")
-    return historic_blob
 
 class TelemetryServer():
     def __init__(self):
@@ -57,10 +21,52 @@ class TelemetryServer():
         # self.udp_dashboard_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.udp_dashboard_sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, self.buf_size)
         self.listen = True
-    
+
+        # History DB
+        self.historic_data = []
+        self.subscribed_keys = {}
+        self.historic_data_max_size = 1000
+
+        # Setup Flask server
+        self.flask_server = Flask(__name__, static_url_path='', static_folder=openmct_dist, template_folder='templates')
+        self.flask_server.config['SECRET_KEY'] = 'secret!'
+        self.socketio = SocketIO(self.flask_server, async_mode="gevent")
+
+        self.setup_event_handlers()
+
     def run_flask(self, browser_port):
         print(f"Running on http://localhost:{browser_port}/index.html")
-        socketio.run(app, debug=False, use_reloader=False, port=browser_port)
+        self.socketio.run(self.flask_server, debug=False, use_reloader=False, port=browser_port)
+    
+    def setup_event_handlers(self):
+        @self.socketio.on("subscribe")
+        def handle_subscribe(key):
+            print(f"Subscribe req: {key}")
+            self.subscribed_keys[key] = True
+
+        @self.socketio.on("unsubscribe")
+        def handle_unsubscribe(key):
+            print(f"Unsubscribe req: {key}")
+            self.subscribed_keys.pop(key, None)
+
+        @self.flask_server.route('/')
+        def static_file():
+            return self.flask_server.send_static_file('index.html')
+
+        @self.flask_server.route('/history/<key>/<start>/<end>/<strategy>/<size>')
+        def handle_historic_data(key, start, end, strategy, size):
+            historic_blob = []
+            for msg_batch in self.historic_data:
+                if msg_batch["timestamp"] > float(start) and msg_batch["timestamp"] < float(end):
+                    for stored_key, value in msg_batch.items(): # Extract the item
+                        if stored_key == key: # Find needed key
+                            if isinstance(value, list): # Handle tuples in historic. Take only the first value
+                                value = value[0]
+                            historic_msg = {"id": stored_key, "value": value, "timestamp": msg_batch["timestamp"], "mctLimitState":None}
+                            # print(f"One of the historic msgs: {historic_msg}")
+                            historic_blob.append(historic_msg)
+            print(f"Send {len(historic_blob)} history items for {key}")
+            return historic_blob
 
     def start_data_listening_thread(self, address_listen_to):
         # Start listening for telemetry
@@ -81,25 +87,25 @@ class TelemetryServer():
             #print(msg_batch, flush=True)
 
             # Emit realtime messages to OpenMCT
-            with app.app_context():
+            with self.flask_server.app_context():
                 # Fetch messages that the openmct is subscribed for
                 for msg_key, msg_value in msg_batch.items():
                     # Handle spaces (replace with "_")
                     msg_key = msg_key.replace(" ", "_")
 
-                    if subscribed_keys.get(msg_key):
+                    if self.subscribed_keys.get(msg_key):
                         # Handle tuples in realtime. Take only the first value
                         if isinstance(msg_value, list):
                             msg_value = msg_value[0]
-                        socketio.emit("realtime", [{"id": msg_key, "value": msg_value, "timestamp":timestamp, "mctLimitState": None}])
+                        self.socketio.emit("realtime", [{"id": msg_key, "value": msg_value, "timestamp":timestamp, "mctLimitState": None}])
             
             # Save history
             msg_batch["timestamp"] = timestamp # append timestamp
-            historic_data.append(msg_batch) # [{"field_1": 1, "field_2": 2, "timestamp":timestamp}, {...}, ...]
+            self.historic_data.append(msg_batch) # [{"field_1": 1, "field_2": 2, "timestamp":timestamp}, {...}, ...]
             
             # Cyclic buffer like
-            if len(historic_data) > historic_data_max_size:
-                historic_data.pop(0)
+            if len(self.historic_data) > self.historic_data_max_size:
+                self.historic_data.pop(0)
 
         
 
