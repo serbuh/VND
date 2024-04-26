7141d35edbc931398fca0a2aba963307d5903bf8
serbuh <ser.buh@gmail.com>
2024-04-26 18:06:25 +0300

    Add load function, clear function

--- telemetry_server/server.py
+++ telemetry_server/server.py
@@ -38,12 +38,13 @@ class TelemetryServer():
         self.historic_data_max_size = 10000
         self.predefined_keys = self.read_predefined_interface(openmct_interface_json_path)
         self.received_keys = []
+        self.dump_folder_name = "saved_experiments"
 
         # Setup Flask server
         self.flask_server = Flask(__name__, static_url_path='', static_folder=openmct_dist, template_folder='templates')
         self.flask_server.config['SECRET_KEY'] = 'secret!'
         self.socketio = SocketIO(self.flask_server, async_mode="gevent")
-
+        
         self.setup_event_handlers()
         
     
@@ -92,7 +93,14 @@ class TelemetryServer():
                 <h1>Experiment</h1>
                 <form action="/save_experiment" method="post">
                     <input type="text" name="save_postfix" placeholder="Experiment name">
-                    <button type="submit">Save</button>
+                    <button type="submit">Save</button><br>
+                </form>
+                <form action="/clear_experiment" method="post">
+                    <button type="submit">Clear</button><br>
+                </form>
+                <form action="/load_experiment" method="post">
+                    <input type="file" name="load_path" id="file">
+                    <button type="submit">Load</button><br>
                 </form>
                 <p>Status: {}</p>
             </body>
@@ -103,8 +111,22 @@ class TelemetryServer():
         def save_experiment_function():
             save_postfix = request.form['save_postfix']
             # Dump historic data to file
-            experiment_name = self.save_historic_data(save_postfix)
-            status = f"Saved experiment: {experiment_name}"
+            status = self.save_historic_data(save_postfix)
+            print(status)
+            return redirect(url_for('control_page', status_message=status))
+        
+        @self.flask_server.route('/clear_experiment', methods=['POST'])
+        def clear_experiment_function():
+            # Dump historic data to file
+            status = self.clear_historic_data()
+            print(status)
+            return redirect(url_for('control_page', status_message=status))
+        
+        @self.flask_server.route('/load_experiment', methods=['POST'])
+        def load_experiment_function():
+            load_path = request.form['load_path']
+            # Load historic data from file
+            status = self.load_historic_data(load_path)
             print(status)
             return redirect(url_for('control_page', status_message=status))
         ##### End of control page functions #####
@@ -190,20 +212,29 @@ class TelemetryServer():
             self.historic_data.append(msg_batch) # [{"field_1": 1, "field_2": 2, "timestamp":timestamp}, {...}, ...]
             
             # Cyclic buffer like
-            if len(self.historic_data) > self.historic_data_max_size:
-                self.historic_data.pop(0)
+            # if len(self.historic_data) > self.historic_data_max_size:
+            #     self.historic_data.pop(0)
     
+    def load_historic_data(self, file_path):
+        full_file_path = os.path.join(self.dump_folder_name, file_path)
+        # Create folder if not exist
+        if not os.path.isfile(full_file_path):
+            return f"No such file {full_file_path}"
+        
+        return f"Loaded file {full_file_path}"
+
+    def clear_historic_data(self):
+        self.historic_data = []
+        return "Cleared history"
+
     def save_historic_data(self, save_postfix=""):
         # Do nothing if no historic data present
         if not len(self.historic_data):
-            return "Empty experiment"
-        
-        # Folder name to save data
-        dump_folder_name = "saved_experiments"
+            return "Empty experiment. Nothing to save"
         
         # Create folder if not exist
-        if not os.path.exists(dump_folder_name):
-            os.makedirs(dump_folder_name)
+        if not os.path.exists(self.dump_folder_name):
+            os.makedirs(self.dump_folder_name)
 
         # Calculate start and stop timestamps
         start_millisec = self.historic_data[0]['timestamp']
@@ -215,14 +246,14 @@ class TelemetryServer():
         if save_postfix:
             save_postfix = "_" + save_postfix
         experiment_name = f"dump_{f_year}-{f_month}-{f_day}__{f_hour}-{f_minute}-{f_sec}__{duration_minutes}_min{save_postfix}.pkl"
-        full_rel_path = os.path.join(dump_folder_name, experiment_name)
+        full_rel_path = os.path.join(self.dump_folder_name, experiment_name)
 
         # Dump history to file
         with open(full_rel_path, 'wb') as f:
             pickle.dump(self.historic_data, f)
 
         # Return experiment name
-        return full_rel_path
+        return f"Saved {full_rel_path}"
             
     def convert_millisec_to_date_string(self, timestamp_milliseconds):
         # Convert milliseconds since epoch to a datetime object
